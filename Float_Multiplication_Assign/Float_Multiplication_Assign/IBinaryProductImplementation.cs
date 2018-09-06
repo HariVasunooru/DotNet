f98395cfce0ae7bb0206b7b2e018fba5ea33c642
hari460134 <hari460134@gmail.com>
2018-09-06 17:35:58 +0530

    Newly Added

---
+++ Float_Multiplication_Assign/Float_Multiplication_Assign/IBinaryProductImplementation.cs
@@ -0,0 +1,10 @@
+﻿namespace Float_Multiplication_Assign
+{
+    interface IBinaryProductImplementation
+    {
+        void BinaryConvertion();
+        int BinaryCondition(int valueOne, int valueTwo);
+        int BinaryProduct(int binaryValueOne, int binaryValueTwo);
+
+    }
+}
